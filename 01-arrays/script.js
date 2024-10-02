fab93a144ca7589fba4f44c5a9c1b9c5daf8bd8b
massimopallera <pallera.massimo@gmail.com>
2024-10-02 15:24:17 +0200

    docs: added comments

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,31 +15,47 @@ console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
+//Didn't use indexOf because I need to change the 5th element, not a specified string
+//With splice() I can change an element content
 teachers.splice(4,1,'Patrick')
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+
+//pop() remove the last element of the array and returns it
 const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+
+//shift() remove the first element of the array and it returns that element
 const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+//push() add the element at the end of the array 
 teachers.push('Vanessa')
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+//unshift() add the element at the start of the array 
 teachers.unshift('Sarah')
 console.log(teachers);
 
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 const isFabioPresent = teachers.indexOf('Fabio');
+
+//indexOf() returns -1 if the element is not in the array
 if (isFabioPresent !== -1){
+
+  //+1 because js is a zero-based language, so indexes start from 0
   console.log(`Fabio è nella lista nella posizione ${isFabioPresent + 1}`)
 } else {
   console.log('Fabio non è nella lista')
@@ -56,6 +72,8 @@ if (lewisIndex !== -1){
 
 // 9. Unisci tutti gli insegnanti nell'array teachers in una stringa
 // separata da virgole e salvala nella variabile teachersString
+
+//join() is used to put all array's elements in a string, u can choose the separator too
 const teachersString = teachers.join(', ');
 console.log(teachersString)
 
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,6 +10,8 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
+
+//reverse function reverse the entire array
 const reversedTeachers = teachers.reverse();
 console.log(reversedTeachers);
 
@@ -19,6 +21,8 @@ console.log(reversedTeachers);
 let longNames = [];
 
 for (let i = 0; i < teachers.length; i++){
+
+  //if the array's element has a length of 5 or more, it will be pushed in longNames
   if (teachers[i].length >= 5){
     longNames.push(teachers[i])
   }
@@ -26,5 +30,8 @@ for (let i = 0; i < teachers.length; i++){
 console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+//without creating a var for indexOf
+//I can use splice() function to remove an element of the array
 teachers.splice(teachers.indexOf('Ed'),1)
 console.log(teachers)
